74b66bc4fe4911d6c2687bfa7f14e7b3f2a6e326
SAMARTH DEVLI <103375834+samarth502@users.noreply.github.com>
2024-06-27 18:19:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ const cartRouter = require('./Router/CartRouter.js')
 // const productRouter = require('./Router/Products.js');
 
 
-const corsOptions = {
-    originL:"http://localhost:5173",
-    methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
-    Credential:true
-  }
+// const corsOptions = {
+//     originL:"http://localhost:5173",
+//     methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
+//     Credential:true
+//   }
 
 
 // Middleware
 // app.use(express.static(path.resolve(_dirname,'dist')))
 app.use(express.static(path.resolve(__dirname,'dist')))
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use('/auth' , userRouter.router);
 app.use('/api/product' , productRouter.router);
@@ -33,12 +33,18 @@ app.use('/api/product' , productRouter.router);
 // cart Router
 app.use('/api/cart',cartRouter.router)
 
-app.get('/',(req,res)=>{
-    app.use(express.static(path.resolve(__dirname,"Front-end","build")));
-    res.sendFile(path.resolve(__dirname,"Front-end","build","index.html"));
+app.get('/',async (req,res)=>{
 
+    try {
+        res.status(200).json({msg:"I am in home route"})
+    } catch(err){
+     res.status(500).json({msg:"Error in home route"})
 
-})
+        
+    }
+    
+
+});
 
 
 main().catch(err => console.log(err));
